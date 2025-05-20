a8e93f5981d1489307a9f09f804ea87dbc535d84
Karolina <karolina.jedrajczyk@gmail.com>
2025-05-20 09:14:07 +0200

    fixes

--- 05/Menu.js
+++ 05/Menu.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export const Menu = ( {items}) => {
-    return (
-        <>
-        {items.map((item, i) => (
-    <li key={i}>
-        <a href={item.url}>{item.text}</a>
-    </li>
-))}
-        </>
-    )
-}
+export const Menu = ({ items }) => {
+  return (
+    <>
+      {items.map((item, i) => (
+        <li key={i}>
+          <a href={item.url}>{item.text}</a>
+        </li>
+      ))}
+    </>
+  );
+};
